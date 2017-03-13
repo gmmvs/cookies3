0f350e362ab9237ed25190707dd405a7e0f49a87
gmmvs <gumamo1617daw2@gmail.com>
2017-03-13 20:38:44 +0100

    cookies3 commit 1

--- index.php
+++ index.php
@@ -1,17 +1,20 @@
 <html> 
-    <head>
-    <meta charset="UTF-8">
+    <head> 
+            <meta charset="UTF-8"> 
         <title>Desplegant una aplicació PHP sobre Heroku</title> 
     </head> 
     <body> 
-        <?php 
-            echo "Afegint Cookies<br>"; 
-            echo "Sóc el desenvolupador gmmvs"; 
-            setcookie("nom", "Jaume Pons", time()+3600, "/", "", 0); 
-            setcookie("edat", "20", time()+3600, "/", "", 0); 
-            setcookie("cicle", "DAW", time()+3600, "/", "", 0); 
-            setcookie("curs", "2n", time()+3600, "/", "", 0); 
-            setcookie("any", "2016­2017", time()+3600, "/", "", 0);            
-        ?>  
+       <?php 
+echo 'Afegint cookies <br>';
+echo 'Sóc el desenvolupador gmmvs';
+setcookie("nom","Jaume Pons", time()+3600, "/","",0);
+setcookie("edat","20", time()+3600, "/","",0);
+setcookie("cicle","Daw", time()+3600, "/","",0);
+setcookie("nom","2n", time()+3600, "/","",0);
+setcookie("any","20162017", time()+3600, "/","",0);
+
+
+?>
+
     </body>    
 </html> 
