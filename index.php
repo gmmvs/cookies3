d2ac17268d5958264e80299a28c969adc30fafe4
gmmvs <gumamo1617daw2@gmail.com>
2017-03-13 20:42:13 +0100

    cookies3 commit 1

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <body> 
        <?php 
 echo 'Afegint cookies <br>';
-echo 'Sóc el desenvolupador gmmvs';
+echo 'Sóc el desenvolupador guillem martinez';
 setcookie("nom","Jaume Pons", time()+3600, "/","",0);
 setcookie("edat","20", time()+3600, "/","",0);
 setcookie("cicle","Daw", time()+3600, "/","",0);
